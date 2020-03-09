d0904773e6bee1502d9499708380199cd050be4d
zchmielewska <zchmielewska@gmail.com>
2020-03-09 07:50:30 +0100

    update vignette

--- tests/testthat/test_dref.R
+++ tests/testthat/test_dref.R
@@ -21,3 +21,22 @@ test_that("dref___y() returns date reference within a year", {
   expect_equal(dref_ldoy("2022-02-14"),  as.Date("2022-12-31"))
   expect_equal(dref_lwdoy("2022-02-14"), as.Date("2022-12-30"))
 })
+
+test_that("dref_mtd() returns month-to-date", {
+  expect_equal(dref_mtd("2020-09-21"), as.Date("2020-08-31"))
+  expect_equal(dref_mtd("2020-03-08"), as.Date("2020-02-29"))
+  expect_equal(dref_mtd("2020-01-20"), as.Date("2019-12-31"))
+})
+
+test_that("dref_qtd() returns quarter-to-date", {
+  expect_equal(dref_qtd("2020-09-21"), as.Date("2020-06-30"))
+  expect_equal(dref_qtd("2020-06-30"), as.Date("2020-03-31"))
+  expect_equal(dref_qtd("2020-03-08"), as.Date("2019-12-31"))
+})
+
+test_that("dref_ytd() returns year-to-date", {
+  expect_equal(dref_ytd("2020-09-21"), as.Date("2019-12-31"))
+  expect_equal(dref_ytd("2020-06-30"), as.Date("2019-12-31"))
+  expect_equal(dref_ytd("2019-12-31"), as.Date("2018-12-31"))
+})
+
