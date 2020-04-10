49475485d9072d09a22aabfb38dbc3d91782be9d
Zuzanna Chmielewska <zuzanna.chmielewska@aaa-riskfinance.pl>
2020-04-10 08:52:44 +0200

    add change_type()

--- R/ircurve.R
+++ R/ircurve.R
@@ -1,20 +1,45 @@
 
-create_ircurve <- function(x = double(), type = "forward", period = "year") {
+as_ircurve <- function(rates = double(), type = "forward", period = "year") {
   type   <- match.arg(type, c("forward", "spot"))
   period <- match.arg(period, c("month", "year"))
   
-  ircurve <- structure(x, 
+  ircurve <- structure(rates, 
                       class = "ircurve",
                       type = type,
                       period = period)
   return(ircurve)
 }
 
-print.ircurve <- function(ircurve) {
-  cat(crayon::silver(paste0("Interest rate curve\nType: ", attributes(ircurve)$type, "\nPeriod: ", attributes(ircurve)$period, "\nRates:\n")))
-  as.vector(ircurve)
+change_type <- function(ircurve, to_type) {
+  if(!inherits(ircurve, "ircurve")) ircurve <- as_ircurve(ircurve)
+  to_type   <- match.arg(to_type, c("forward", "spot"))
+  from_type <- attributes(ircurve)$type
+  
+  if(from_type == to_type) return(ircurve)
+  
+  if(from_type == "forward" & to_type == "spot") {
+    forward <- ircurve
+    spot    <- as_ircurve(rates = 0)
+    attributes(spot)$type <- to_type
+    
+    for (n in seq_along(forward)) {
+      spot[n] <- prod(1+forward[1:n])^(1/n)-1
+    }
+    return(spot)
+  }
+  
+  if(from_type == "spot" & to_type == "forward") {
+    spot    <- ircurve
+    forward <- as_ircurve(rates = 0)
+    attributes(forward)$type <- to_type
+    
+    for (n in seq_along(spot)) {
+      if(n == 1) {
+        forward[n] <- spot[n]
+      } else {
+        forward[n] <- (1+spot[n])^(n)/prod(1+forward[1:(n-1)]) - 1
+      }
+    }
+    return(forward)
+  }
 }
-
-ircurve <- create_ircurve(rep(0.01, 10))
-
-print(ircurve)
