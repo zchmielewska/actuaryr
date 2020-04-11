6febb8bb49a9e14cd7ed2e713ce20675891b4d1f
Zuzanna <zchmielewska@gmail.com>
2020-04-11 16:58:12 +0200

    add ircurve functions

--- R/ircurve.R
+++ R/ircurve.R
@@ -1,4 +1,3 @@
-
 as_ircurve <- function(rates = double(), type = "forward", period = "year") {
   type   <- match.arg(type, c("forward", "spot"))
   period <- match.arg(period, c("month", "year"))
@@ -52,24 +51,78 @@ change_period <- function(ircurve, to_period) {
   
   if(from_period == to_period) return(ircurve)
   
-  
-  ### In development
-  if(to_period == "year"  & type == "forward") { 
-    # check if divideable by 12
-    x <-c(rep(0.1, 12), 0.2)
-    if((length(x) %% 12) != 0) {
+  # from = month; to = year | forward
+  if(to_period == "year"  & type == "forward") {
+    ircurve_month <- ircurve
+    
+    # curve must include full year data
+    if((length(ircurve_month) %% 12) != 0) {
       warning(paste0("The last available rate has been assumed for the months remaining to full year."))
-      y <- append(x, rep(x[length(x)], 12 - (length(x) %% 12)))
-      length(y)
+      last_rate <- ircurve_month[length(ircurve_month)]
+      remaining_months <- 12 - (length(ircurve_month) %% 12)
+      ircurve_month <- append(ircurve_month, rep(last_rate, remaining_months))
     }
     
-  
-      
+    no_bins    <- length(ircurve_month)/12
+    start_bins <- (0:(no_bins-1))*12 +1
+    end_bins   <- (1:no_bins)*12
     
+    rates_year <- c()
+    for(i in 1:no_bins) {
+      rates_year[i] <- prod(1+ircurve_month[start_bins[i]:end_bins[i]])-1
+    }
+    
+    result <- as_ircurve(rates_year, type = "forward", period = "year")
   }
-  if(to_period == "year"  & type == "spot") { }
-  if(to_period == "month" & type == "forward") { }
-  if(to_period == "month" & type == "spot") { }
-  ### In development
   
+  # from = year; to = month | forward
+  if(to_period == "month" & type == "forward") { 
+    ircurve_year <- ircurve
+    
+    rates_month <- c()
+    for(i in seq_along(ircurve_year)) {
+      monthly_rate <- (1+ircurve_year[i])^(1/12)-1
+      rates_month[(1:12)+12*(i-1)] <- rep(monthly_rate, 12)
+    }
+    
+    result <- as_ircurve(rates_month, type = "forward", period = "month")
+  }
+  
+  # from = month; to = year | spot
+  if(to_period == "year"  & type == "spot") { 
+    ircurve_month <- ircurve
+    
+    # curve must include full year data
+    if((length(ircurve_month) %% 12) != 0) {
+      warning(paste0("The last available rate has been assumed for the months remaining to full year."))
+      last_rate <- ircurve_month[length(ircurve_month)]
+      remaining_months <- 12 - (length(ircurve_month) %% 12)
+      ircurve_month <- append(ircurve_month, rep(last_rate, remaining_months))
+    }
+    
+    no_bins    <- length(ircurve_month)/12
+    end_bins   <- (1:no_bins)*12
+    
+    rates_year <- c()
+    for(i in 1:no_bins) {
+      rates_year[i] <- (1+ircurve_month[end_bins[i]])^(12)-1
+    }
+    
+    result <- as_ircurve(rates_year, type = "spot", period = "year")
+  }
+  
+  # from = year; to = month | spot
+  if(to_period == "month" & type == "spot") { 
+    stop("Not developed yet.")
+    ircurve_year <- ircurve
+    
+    rates_month <- c()
+    for(i in seq_along(ircurve_year)) {
+      monthly_rate <- (1+ircurve_year[i])^(1/12)-1
+      rates_month[(1:12)+12*(i-1)] <- rep(monthly_rate, 12)
+    }
+    
+    result <- as_ircurve(rates_month, type = "spot", period = "month")
+  }
+  return(result)
 }
